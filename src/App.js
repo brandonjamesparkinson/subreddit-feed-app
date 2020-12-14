8f71bad6f7397831ae9f40977f40f94fe2bb482b
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-12-14 14:50:11 +0000

    + removed unused react setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json")
+  }, [subreddit]);
+
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <input type="text" className="input" value="webdev"/> 
 
       </header>
+      <div className="articles">
+        
+      </div>
     </div>
   );
 }
