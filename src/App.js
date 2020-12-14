24e20c15d6873408a68eeefbc7b4dbf133ef25a8
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-12-14 15:24:21 +0000

    + added titles and redirects to subreddit links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {useState, useEffect} from 'react';
 
+import Article from './Components/Article';
+
 function App() {
   const [articles, setArticles] = useState([]);
   const [subreddit, setSubreddit] = useState('webdev');
 
   useEffect(() => {
-    fetch("https://www.reddit.com/r/webdev.json")
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if  (res.status != 200){
+        console.log("ERROR");
+        return;
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);
+        }
+      });
+    })
   }, [subreddit]);
 
   return (
@@ -15,6 +28,8 @@ function App() {
 
       </header>
       <div className="articles">
+        {(articles != null) ? articles.map((article, index) => <Article key ={index} article = {article.data}/>) : ''}
+
         
       </div>
     </div>
